d7fe6436aa6f1224cf56374cff81e34aa20dea38
Den <denissvet.code@gmail.com>
2016-10-24 12:10:25 +0300

    Lectia 51

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header.component';
+import { PostsComponent } from './posts/posts.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
+import { PostItemComponent } from './posts/post-list/post-item.component';
+import { PostDetailComponent } from './posts/post-detail/post-detail.component';
+import { DropdownDirective } from './dropdown.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PostsComponent,
+    PostListComponent,
+    PostItemComponent,
+    PostDetailComponent,
+    DropdownDirective
   ],
   imports: [
     BrowserModule,
