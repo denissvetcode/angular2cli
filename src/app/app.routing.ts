3008d39f9ccc0ab9065243ef7349da64e46d8398
Den <denissvet.code@gmail.com>
2016-10-27 18:07:24 +0300

    lectia 103

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,7 +2,6 @@
  * Created by Den on 10/24/2016.
  */
 import {Routes, RouterModule} from "@angular/router";
-import {AppComponent} from "./app.component";
 import {PostsComponent} from "./posts/posts.component";
 import {POST_ROUTES} from "./posts/posts.routes";
 
